2e2f8b4bf59b3ad36ee42a4db1a62c9ed9aed782
Blackstorm <blackstorme07.bkr07@gmail.com>
2024-11-27 21:51:21 +0100

    add deplacement function (need to be tested)

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import socket
+
 pions_blanc = [[1 if (i + j) % 2 == 0 else -1 for i in range(10)] for j in range(4)]
 grille_vide = [[0 if (i + j) % 2 == 0 else -1 for i in range(10)] for j in range(2)]
 pions_noir = [[-2 if (i + j) % 2 == 0 else -1 for i in range(10)] for j in range(4)]
-
-# pions_blanc.append(grille_vide)
-# pions_blanc.append(pions_noir)
-
 grille_init = pions_blanc + grille_vide + pions_noir
 print(grille_init)
 
@@ -20,5 +17,18 @@ grille_test = [
 [-1, -2, -1, -2, -1, -2, -1, -2, -1, -2], 
 [-2, -1, -2, -1, -2, -1, -2, -1, -2, -1], 
 [-1, -2, -1, -2, -1, -2, -1, -2, -1, -2]
-
 ]
+
+def deplacement(grid, x, y, direction, lenght):
+    
+    left_value = grid[x-lenght][y-lenght]
+    right_value = grid[x-lenght][y+lenght]
+    if left_value == 0 or right_value == 0:
+        if direction == 'g':
+            grid[x][y], grid[x-lenght][y-lenght] = grid[x-lenght][y-lenght],\
+                grid[x][y]
+        if direction == 'd':
+            grid[x][y], grid[x-lenght][y+lenght] = grid[x-lenght][y+lenght],\
+                grid[x][y]
+    else:
+        print("déplacement impossible")
